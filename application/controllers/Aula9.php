fcd18f46b184d83bfc991952b44203d3f5d455ad
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2023-11-28 14:21:52 -0300

    10 input class

---
+++ application/controllers/Aula9.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Aula9 extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->library('email');
+
+        $configEmail['protocol'] = 'smtp';
+
+        $this->email->initialize($configEmail);
+
+        $this->email->from('andrecarmonadev@gmail.com', 'Andre Carmona');
+        $this->email->reply_to('andrecarmonadev@gmail.com', 'Andre Carmona');
+        $this->email->to('andrecarmonadev@gmail.com');
+        $this->email->cc('andrecarmonadev@gmail.com');
+        $this->email->bcc('andrecarmonadev@gmail.com');//   Copia oculta
+
+        $this->email->subject('Email de teste');
+        $this->email->message('Este é o conteudo do email.');
+        $this->email->set_alt_message('Esta é a msg alternativa');
+
+        $this->email->attach($_FILES['upload']['tmp_name']);
+        $this->email->attach($_FILES['upload']['tmp_name']);
+        $this->email->attach($_FILES['upload']['tmp_name']);
+
+        if($this->email->send()) {
+            echo 'Email n enviado.';
+            $this->email->print_debugger();
+        }
+        $this->email->clear(); 
+    }
+}
